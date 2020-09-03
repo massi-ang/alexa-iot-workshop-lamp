9ffca36437881370d24f412ae100b026168831a1
Massimiliano Angelino <angmas@amazon.com>
2020-09-03 10:56:46 +0200

    Moved to aws-device-sdk-js-v2

--- index.js
+++ index.js
@@ -1,69 +1,66 @@
-const AWSIoT = require('aws-iot-device-sdk');
+const AwsIot = require('aws-iot-device-sdk-v2');
 const QRCode = require('qrcode');
 const config = require('./config')
 
+async function main () {
+  console.log('Smart lamp simulator');
+  try {
+    config_builder = AwsIot.iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder_from_path(
+      './credentials/cert.pem',
+      './credentials/private.key');
+    const timer = setTimeout(() => { }, 60 * 1000);
+    const client_bootstrap = new AwsIot.io.ClientBootstrap();
 
-console.log('Smart lamp simulator');
+    config_builder.with_clean_session(true);
+    config_builder.with_client_id(config.thingName);
+    config_builder.with_endpoint(config.iotEndpoint);
 
-const shadow = AWSIoT.thingShadow({
-  keyPath: 'credentials/private.key',
-  caPath: 'credentials/rootCA.pem',
-  certPath: 'credentials/cert.pem',
-  clientId: config.thingName,
-  host: config.iotEndpoint
-});
+    const mqtt_config = config_builder.build();
+    const client = new AwsIot.mqtt.MqttClient(client_bootstrap);
+    const connection = client.new_connection(mqtt_config);
+    const shadow = new AwsIot.iotshadow.IotShadowClient(connection);
+    await connection.connect();
+    console.log('Connected to IoT Core...\n');
+    await shadow.subscribeToShadowDeltaUpdatedEvents({
+      thingName: config.thingName
+    }, 1, (error, event) => {
+      //  console.debug(event);
+      const desiredPowerState = event.state.powerState;
+      const reportedState = {
+        reported: {
+          powerState: desiredPowerState
+        }
+      };
 
-let clientTokenUpdate;
+      shadow.publishUpdateShadow({ thingName: config.thingName, state: reportedState }, 1);
 
-
-shadow.on('connect', function () {
-  console.log('Connected');  
-  shadow.register(config.thingName, {}, function () {
+      console.info(`The Smart Lamp is now ${desiredPowerState}`)
+    });
 
     const initState = {
-      state: {
-        reported: {
-          powerState: "OFF"
-        }
+      reported: {
+        powerState: "OFF"
       }
     };
 
-    clientTokenUpdate = shadow.update(config.thingName, initState);
+    await shadow.publishUpdateShadow({ thingName: config.thingName, state: initState }, 1);
 
-    if (clientTokenUpdate === null) {
-      console.log('update shadow failed, operation still in progress');
-    }
-
-    console.info('connected to IoT Core...\n');
 
     console.info('This is the QR Code shipped with the Device:');
     let url = `${config.deviceBindingUrl}device/${config.thingName}`
-        QRCode.toString(url, {type: 'terminal'}, function (err, string) {
+    QRCode.toString(url, { type: 'terminal' }, function (err, string) {
       if (err) throw err;
-          console.log(string)
-          console.log(`Browse to ${url} to register`)
+      console.log(string)
+      console.log(`Browse to ${url} to register`)
+    })
 
-        })
-    
-  })
+    setInterval(() => { }, 60);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
-});
 
-shadow.on('delta', function (thingName, stateObject) {
-  const desiredPowerState = stateObject.state.powerState;
-  const reportedState = {
-    state: {
-      reported: {
-        powerState: desiredPowerState
-      }
-    }
-  };
+}
 
-  shadow.update(config.thingName, reportedState);
-
-  console.info(`turn ${desiredPowerState} Smart Lamp`)
-});
-
-shadow.on('error', (err) => { console.log(err) });
-
-shadow.on('offline', () => { console.log('Disconnected') })
+main()
